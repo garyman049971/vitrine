21e5ef58086f53125355b19948144c1771df6b61
garyman049971 <garyman049@gmail.com>
2025-01-03 11:27:10 -0400

    modif de la page apropos

---
+++ src/Entity/Contact.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContactRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ContactRepository::class)]
+class Contact
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Prenom = null;
+
+    #[ORM\Column]
+    private ?int $Telephone = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Sujet = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Message = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): static
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->Prenom;
+    }
+
+    public function setPrenom(string $Prenom): static
+    {
+        $this->Prenom = $Prenom;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?int
+    {
+        return $this->Telephone;
+    }
+
+    public function setTelephone(int $Telephone): static
+    {
+        $this->Telephone = $Telephone;
+
+        return $this;
+    }
+
+    public function getSujet(): ?string
+    {
+        return $this->Sujet;
+    }
+
+    public function setSujet(string $Sujet): static
+    {
+        $this->Sujet = $Sujet;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->Message;
+    }
+
+    public function setMessage(string $Message): static
+    {
+        $this->Message = $Message;
+
+        return $this;
+    }
+}
